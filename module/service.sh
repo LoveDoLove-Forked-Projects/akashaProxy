55769c7128f0952d44aa273d743693894ee96f86
Elysia <118418714+elysias123@users.noreply.github.com>
2025-06-28 22:16:40 +0800

    尝试解决启动问题

--- module/service.sh
+++ module/service.sh
@@ -1,7 +1,13 @@
 #!/system/bin/sh
 . /data/clash/clash.config
 
+
+
+if [ ! -d ${Clash_run_path} ]; then
+    mkdir -p ${Clash_run_path}
+fi
 crond -c ${Clash_run_path}
+chmod -R 770 ${Clash_data_dir}
 
 if [ ${self_start} == "true" ] ; then
     nohup /data/clash/scripts/clash.service -s && /data/clash/scripts/clash.iptables -s
